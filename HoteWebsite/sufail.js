69f7bd0b6596481644b1df95ea941dc9199a32ac
sufail-dev <myofficialthing@gmail.com>
2020-11-22 14:23:03 +0530

    first commit

---
+++ HoteWebsite/sufail.js
@@ -0,0 +1,18 @@
+
+
+  let my={
+       NameIs:"Sufail S",
+       ProfessionIs:"Software developer In Test",
+       InterestsAre:["Front-End develpment","Test Automation"],
+       Skills:["HTML","CSS","JavaScript","Java","Selenium","Cucumber","RestAPI","TestNg",
+       "ReactJs","C#"]
+       }
+  for (let value of Object.values(my)) {
+       console.log(value); 
+     }
+
+
+
+
+
+     
